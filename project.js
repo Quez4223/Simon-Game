2e5797b6e32940e436cba511bf087114841c4b39
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-02-09 21:03:48 -0500

    Adding jQuery

--- project.js
+++ project.js
@@ -1,5 +1,18 @@
+// Got the idea to use this set up of code from https://www.youtube.com/watch?v=0n3ez0vTAMs
+
 const panelColors = ['green', 'red', 'indigo', 'yellow']
 
 const gamePattern = []
 
-const 
+const userClickedPattern = []
+
+let start = false
+
+$( () => {
+    const $panel = $('#panel')
+    
+    const addText = () => {
+        $('body').append('Button has been clicked')
+    }    
+    })
+     
