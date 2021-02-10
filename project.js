c50e704b74565adca3436e76345d9fc327b01e85
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-02-10 11:51:53 -0500

    colors and buttons are synched

--- project.js
+++ project.js
@@ -10,13 +10,32 @@ let start = false
 
 
 $( () => {
-    $('.panel').on('click', ()=> {
+    $('.panel').on('click', (event)=> {
         // console.log('panel has been clicked')
         const $panelColor = panelColors
+        if ($(event.currentTarget).hasClass('panel top-left')){
+            console.log(panelColors[0])
+        } else {
+            if ($(event.currentTarget).hasClass('panel top-right')){
+                console.log(panelColors[1])
+            } else {
+                if ($(event.currentTarget).hasClass('panel bottom-left')){
+                    console.log(panelColors[2])
+                } else {
+                    if ($(event.currentTarget).hasClass('panel bottom-right')){
+                        console.log(panelColors[3])
+                    }
+                }
+            }
 
-        if ()
+        }
+        
+    
     })
 
+    
+    
+    
     
     $('button').on('click', ()=>{
         console.log('button has been clicked')
