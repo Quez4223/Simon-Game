4997e7288b77c80f54f4ca61718d9fef3da34438
MarQuez LeDay <quezleday@MarQuezs-MBP.attlocal.net>
2021-02-11 12:09:48 -0500

    Pseudo coded winning condition

--- project.js
+++ project.js
@@ -6,7 +6,9 @@ const gamePattern = []
 
 const userClick = 0
 
-const noPress = true
+const started = false
+
+const level = 0
 
 
 $(() => {
@@ -23,11 +25,8 @@ $(() => {
                 } else {
                     if ($(event.currentTarget).hasClass('panel bottom-right')) {
                         console.log(panelColors[3])
-                    } else {
-                        if ($(event.currentTarget).hasClass('panel')) {
-                            console.log(panelColors).blink(500)
-                        }
-                    }
+                    } 
+                    
                 } 
             }
 
@@ -41,20 +40,38 @@ $(() => {
         gamePattern.push(panelColors[$gamePattern])
         console.log($gamePattern)
         console.log(gamePattern)
+
+        
     })
 
-
-    simonSays() {
-        noPress = true
-        let delay
-        if (gamePattern.length > 10) {
-            delay = 400
-        }
-        const pause = setTimeout() {
-            for (let i = 0; i < gamePattern.length; i++) {
-            
-            }
-        }
-    }
-
 }) 
+
+
+
+// opening move after click start
+// simonStart() {
+//     $gamePattern
+//     userClick = 0
+// }
+
+
+
+// winning condition idea 
+//  winningGamePattern() { 
+//     let times = 0;
+//       const blinkTop = setTimeout(function() {
+//         $('#panel top-left').effect('bounce', 1);
+//         $('#panel top-right').effect('bounce', 1);
+//       }, 250);
+//       const blinkBottom = setTimeout(function() {
+//         $('#panel bottom-left').effect('bounce', 1);
+//         $('#panel bottom-right').effect('bounce', 1);
+//       }, 250);
+//       if (times > _) {
+//         const newGame = setTimeout(function() { 
+//           simonStart();
+//         }, );
+//       }
+//     }, );
+
+// }
